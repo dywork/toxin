9a2f063269bef2486e0a778843953baae80a42fe
dywork <dubrovskywork@gmail.com>
2021-06-29 17:02:59 +0500

    #37 update class name in date-picker

--- src/components/date-picker/utils/calendarClassName.js
+++ src/components/date-picker/utils/calendarClassName.js
@@ -1,10 +1,13 @@
 const calendarClassName = {
   datePicker: 'date-picker',
   datePickerOpened: 'date-picker_opened',
+  datePickerWithError: 'date-picker_with-error',
   wrap: 'date-picker__wrap',
   control: 'date-picker__control',
   button: 'date-picker__button',
   buttonAccent: 'date-picker__button_accent',
+  jsButtonClear: 'js-clear-button',
+  jsButtonAccept: 'js-accept-button',
   sliderButton: 'date-picker__slider-btn',
   sliderButtonPrev: 'date-picker__slider-btn_prev',
   sliderButtonNext: 'date-picker__slider-btn_next',
@@ -16,6 +19,10 @@ const calendarClassName = {
   calendarDayLower: 'date-picker__day_lower',
   calendarDayNotCurrent: 'date-picker__day_not-current',
   calendarDayCurrent: 'date-picker__day_current',
+  calendarDaySelected: 'date-picker__day_selected',
+  calendarDaySelectedStart: 'date-picker__day_selected-start',
+  calendarDaySelectedEnd: 'date-picker__day_selected-end',
+  calendarDaySelectedSpace: 'date-picker__day_selected-space',
   calendarDayWithError: 'date-picker__day_with-error',
 };
 
