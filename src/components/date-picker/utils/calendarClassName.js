321501568e655756144d4b06ba2dfd2dc73e80d5
dywork <dubrovskywork@gmail.com>
2021-06-29 15:46:01 +0500

    #37 moved utils

---
+++ src/components/date-picker/utils/calendarClassName.js
@@ -0,0 +1,22 @@
+const calendarClassName = {
+  datePicker: 'date-picker',
+  datePickerOpened: 'date-picker_opened',
+  wrap: 'date-picker__wrap',
+  control: 'date-picker__control',
+  button: 'date-picker__button',
+  buttonAccent: 'date-picker__button_accent',
+  sliderButton: 'date-picker__slider-btn',
+  sliderButtonPrev: 'date-picker__slider-btn_prev',
+  sliderButtonNext: 'date-picker__slider-btn_next',
+  title: 'date-picker__title',
+  calendar: 'date-picker__calendar',
+  calendarHead: 'date-picker__calendar-head',
+  calendarBody: 'date-picker__calendar-body',
+  calendarDay: 'date-picker__day',
+  calendarDayLower: 'date-picker__day_lower',
+  calendarDayNotCurrent: 'date-picker__day_not-current',
+  calendarDayCurrent: 'date-picker__day_current',
+  calendarDayWithError: 'date-picker__day_with-error',
+};
+
+export default calendarClassName;
