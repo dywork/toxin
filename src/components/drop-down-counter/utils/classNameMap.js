fe63eccd0705803d16bebd5014eb2eb801605a16
dywork <dubrovskywork@gmail.com>
2021-07-14 17:31:30 +0500

    #37 adding classNameMap for dropdown

---
+++ src/components/drop-down-counter/utils/classNameMap.js
@@ -0,0 +1,18 @@
+const classNameMap = {
+  wrap: 'drop-down-counter__wrap',
+  countList: 'drop-down-counter__count-list',
+  countItem: 'drop-down-counter__count-item',
+  countItemName: 'drop-down-counter__count-item-name',
+  counterMenu: 'drop-down-counter__counter-menu',
+  counterButton: 'drop-down-counter__counter-btn',
+  counterButtonMinus: 'drop-down-counter__counter-btn_minus',
+  counterButtonPlus: 'drop-down-counter__counter-btn_plus',
+  counterButtonDisabled: 'drop-down-counter__counter-btn_disabled',
+  counterButtonHidden: 'drop-down-counter__button_hidden',
+  selectView: 'drop-down-counter__select-view',
+  control: 'drop-down-counter__control',
+  button: 'drop-down-counter__button',
+  buttonAccent: 'drop-down-counter__button_accent',
+};
+
+export default classNameMap;
