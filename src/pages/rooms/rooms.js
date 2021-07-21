4a77aa9e2b90effc045cde8fdf24155859addc94
dywork <dubrovskywork@gmail.com>
2021-07-21 12:42:50 +0500

    #37 rename HotelsCard.js

--- src/pages/rooms/rooms.js
+++ src/pages/rooms/rooms.js
@@ -1,6 +1,6 @@
 import '../../style/main.scss';
 import './rooms.scss';
 import '../../components/range-slider/range-slider';
-import '../../components/hotel-card/HotelsCard';
+import '../../components/hotel-card/hotel-card';
 import '../../components/main-menu/MainMenuInit';
 import './sections/filter/filter-init';
