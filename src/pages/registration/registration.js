0104d13995a1f8d0175dc2bbf2d5a984877df593
dywork <dubrovskywork@gmail.com>
2021-08-04 13:59:35 +0500

    #38 moved register card script to class

--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -1,4 +1,4 @@
 import '../../style/main.scss';
 import './registration.scss';
 import '../../components/main-menu/MainMenuInit';
-import '../../components/register-card/register-card';
+import '../../components/register-card';
